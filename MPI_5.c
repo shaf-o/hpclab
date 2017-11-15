3c73dce9f9a14fcb5a7bf50b0f17e96e445b196b
shafiq <syedshafiq29@gmail.com>
2017-11-15 16:13:05 +0530

    Update MPI_5.c

--- MPI_5.c
+++ MPI_5.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
-#include <mpi.h>
-#include <string.h>
-#define BUFFER_SIZE 32
+#include "mpi.h"
 
-int main(int argc,char *argv[])
-{
-	int  MyRank,Numprocs, Destination, iproc;
-	int  tag = 0;
-	int  Root = 0, temp = 1;
-	char Message[BUFFER_SIZE];
-	MPI_Init(&argc,&argv);
-	MPI_Status status;
+int main (int argc, char *argv[])
+{ 
+	 int rank, i;
 
-	MPI_Comm_rank(MPI_COMM_WORLD,&MyRank);
-	MPI_Comm_size(MPI_COMM_WORLD,&Numprocs);
+ 	  MPI_Init (&argc, &argv);
 
-	/* print host name, and send message from process with rank 0 to all other processes */
-	if(MyRank == 0)        {
-		system("hostname");
-		strcpy(Message, "Hello India"); 
-		for (temp=1; temp<Numprocs;temp++)
-		{
-			MPI_Send(Message, BUFFER_SIZE, MPI_CHAR, temp, tag,MPI_COMM_WORLD);
-		}
-	}
- 	else {          
-		system("hostname");
-	   	MPI_Recv(Message, BUFFER_SIZE, MPI_CHAR, Root, tag,MPI_COMM_WORLD, &status);
-		printf("\n%s in process with rank %d from Process with rank %d\n", Message,MyRank,Root);
-  	}
+   MPI_Comm_rank (MPI_COMM_WORLD, &rank);
 
-  	MPI_Finalize();
+
+   if (rank == 0) i = 27;
+
+   MPI_Bcast ((void *)&i, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+   printf ("[%d] i = %d\n", rank, i);
+
+   // Wait for every process to reach this code
+
+
+   MPI_Barrier (MPI_COMM_WORLD);
+
+   MPI_Finalize();
+
+
+   return 0;
+
+
 }
-
-
